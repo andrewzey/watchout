d82f57377d81d5a64ba9ac11b95c0ea64948362b
Hack Reactor Students <andrew@andrewzey.com>
2014-06-13 17:00:26 -0700

    Added enemy generator

--- watchout.js
+++ watchout.js
@@ -18,10 +18,40 @@ var axes = {
   y: d3.scale.linear().domain([0,100]).range([0, gameOptions.height])
 };
 
+
 //Draw the board
-var board = d3.selectAll('.gameboard').append('svg')
+var board = d3.select('.gameboard').append('svg:svg')
           .attr('width', gameOptions.width)
           .attr('height', gameOptions.height);
+
+var createEnemies = function(){
+  return _.range(0, gameOptions.enemyCount).map(function(i){
+    return {
+      id: i,
+      x: Math.random() * 100,
+      y: Math.random() * 100
+    };
+  });
+};
+
+var render = function( enemy_data ){
+  var enemies = board.selectAll('svg').data(enemy_data, function(d) { return d.id; });
+  console.log(enemies);
+  enemies.enter()
+    .append('svg:circle')
+    .attr("class", "enemy")
+    .attr('cx', function ( enemy ){
+      return axes.x(enemy.x);
+    })
+    .attr('cy', function ( enemy ){
+      return axes.y(enemy.y);
+    })
+    .attr('r', 15);
+    enemies.exit().remove();
+};
+
+
+
 
 //Update Scores
 var updateCurrScore = function(){
@@ -34,6 +64,80 @@ var updateHighScore = function(){
   }
 };
 
+var play = function(){
+  var newEnemyPositions = createEnemies();
+  return render(newEnemyPositions);
+
+};
+play();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var Player = function(){
+//   //this.setupDragging = __bind(this.setupDragging, this);
+
+
+// };
+
+
+// Player.prototype.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+// Player.prototype.fill = "#15aaff";
+// Player.prototype.x = 0;
+// Player.prototype.y = 0;
+// Player.prototype.angle = 0;
+// Player.prototype.r = 5;
+// Player.prototype.gameOptions = gameOptions;
+// Player.prototype.render = function( to ){
+//   this.el = to.append('svg:path')
+//     .attr('d', this.path)
+//     .attr('fill', this.fill);
+// };
+
+// Player.prototype.getX = function( meters ){
+
+// };
+// Player.prototype.setX = function( meters ){};
+
+// Player.prototype.getY = function( meters ){};
+// Player.prototype.setY = function( meters ){};
+
+// // fill
+// // x
+// // y
+// // angle
+// r
 
 
 
